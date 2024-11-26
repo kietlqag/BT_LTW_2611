39555feb7df5326842b64fd4ca18146a3a01783b
Le Quoc Kiet <kietlqag@gmail.com>
2024-11-26 14:18:05 +0700

    first commit

---
+++ src/main/java/vn/lqk/Bt2611Application.java
@@ -0,0 +1,13 @@
+package vn.lqk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bt2611Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bt2611Application.class, args);
+	}
+
+}
---
+++ src/test/java/vn/lqk/Bt2611ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.lqk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bt2611ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
